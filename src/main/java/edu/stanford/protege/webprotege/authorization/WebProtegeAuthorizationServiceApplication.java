76966be2a5592c22b3591a22089d17dbcc014c38
Matthew Horridge <matthew.horridge@stanford.edu>
2021-11-05 15:11:29 -0700

    Renamed application

--- src/main/java/edu/stanford/protege/webprotege/authorization/WebProtegeAuthorizationServiceApplication.java
+++ src/main/java/edu/stanford/protege/webprotege/authorization/WebProtegeAuthorizationServiceApplication.java
@@ -1,14 +1,10 @@
 package edu.stanford.protege.webprotege.authorization;
 
 import edu.stanford.protege.webprotege.ipc.WebProtegeIpcApplication;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
-
-import java.util.List;
 
 @SpringBootApplication
 @Import(WebProtegeIpcApplication.class)
