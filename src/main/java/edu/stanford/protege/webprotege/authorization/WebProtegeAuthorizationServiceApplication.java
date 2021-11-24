f182a67f9415a768296bf5e2a44b781fef7ff54c
Matthew Horridge <matthew.horridge@stanford.edu>
2021-11-24 14:24:10 -0600

    Remove mongo Java configuration

--- src/main/java/edu/stanford/protege/webprotege/authorization/WebProtegeAuthorizationServiceApplication.java
+++ src/main/java/edu/stanford/protege/webprotege/authorization/WebProtegeAuthorizationServiceApplication.java
@@ -1,14 +1,21 @@
 package edu.stanford.protege.webprotege.authorization;
 
 import edu.stanford.protege.webprotege.ipc.WebProtegeIpcApplication;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 
+import java.util.Arrays;
+
 @SpringBootApplication
 @Import(WebProtegeIpcApplication.class)
 public class WebProtegeAuthorizationServiceApplication {
+
+	@Autowired
+	AccessManager accessManager;
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebProtegeAuthorizationServiceApplication.class, args);
