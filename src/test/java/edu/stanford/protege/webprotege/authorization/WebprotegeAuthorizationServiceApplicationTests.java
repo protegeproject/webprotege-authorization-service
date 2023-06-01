dabd838247ada1aafd57729db3d82af4a1002d04
Matthew Horridge <matthew.horridge@stanford.edu>
2023-06-01 09:11:38 -0700

    Requires MongoTestExtension and PulsarTestExtension

--- src/test/java/edu/stanford/protege/webprotege/authorization/WebprotegeAuthorizationServiceApplicationTests.java
+++ src/test/java/edu/stanford/protege/webprotege/authorization/WebprotegeAuthorizationServiceApplicationTests.java
@@ -1,9 +1,11 @@
 package edu.stanford.protege.webprotege.authorization;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@ExtendWith({MongoTestExtension.class, PulsarTestExtension.class})
 class WebProtegeAuthorizationServiceApplicationTests {
 
 	@Test
