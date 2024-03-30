4f05b922435c0a95c6dee8177af564a8829b2c61
Matthew Horridge <matthew.horridge@stanford.edu>
2024-03-30 10:30:05 -0700

    Fixes #2

--- src/test/java/edu/stanford/protege/webprotege/authorization/RoleOracleImpl_Tests.java
+++ src/test/java/edu/stanford/protege/webprotege/authorization/RoleOracleImpl_Tests.java
@@ -3,6 +3,9 @@ package edu.stanford.protege.webprotege.authorization;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.Collection;
+import java.util.Collections;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 /**
@@ -24,5 +27,11 @@ public class RoleOracleImpl_Tests {
     void shouldContainRoleAncestors() {
         var closure = roleOracle.getRoleClosure(BuiltInRole.PROJECT_EDITOR.getRoleId());
         assertThat(closure).anyMatch(role -> role.roleId().equals(BuiltInRole.PROJECT_VIEWER.getRoleId()));
+    }
+
+    @Test
+    void shouldGetActionsAssignedToRoles() {
+        var actions = roleOracle.getActionsAssociatedToRoles(Collections.singleton(BuiltInRole.PROJECT_CREATOR.getRoleId()));
+        assertThat(actions.contains(BuiltInAction.CREATE_EMPTY_PROJECT.getActionId()));
     }
 }
